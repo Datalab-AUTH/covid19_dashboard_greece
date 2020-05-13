88a58da929be1aa5f97fdd4359fee4bb291bd9ef
George Vlahavas <vlahavas@gmail.com>
2020-05-14 00:29:12 +0300

    Show all info on table

--- sections/content_west_macedonia/summary_table.R
+++ sections/content_west_macedonia/summary_table.R
@@ -1,6 +1,10 @@
 output$summary_table_west_macedonia <- renderUI({
   tabBox(
-    tabPanel("Κρούσματα ανά Νοσοκομείο",
+    tabPanel(paste0(
+      "Κρούσματα ανά Νοσοκομείο (",
+      format(input$timeslider_west_macedonia, format="%d/%m/%Y"),
+      ")"
+      ),
              div(
                dataTableOutput("summaryDT_west_macedonia"),
                style = "margin-top: -10px")
@@ -9,7 +13,7 @@ output$summary_table_west_macedonia <- renderUI({
   )
 })
 
-output$summaryDT_west_macedonia <- renderDataTable(getSummaryDT_west_macedonia(data_atDate_west_macedonia("2020-05-11")))
+output$summaryDT_west_macedonia <- renderDataTable(getSummaryDT_west_macedonia(data_atDate_west_macedonia(input$timeslider_west_macedonia)))
 proxy_summaryDT_west_macedonia  <- dataTableProxy("summaryDT_west_macedonia")
 
 observeEvent(input$timeslider_west_macedonia, {
@@ -22,13 +26,25 @@ observeEvent(input$timeslider_west_macedonia, {
 summariseData_west_macedonia <- function(df) {
   df %>%
     ungroup() %>%
-    select("hospital_name_el_short", "hospitalized_current", "home_restriction_current", "new_recoveries", "new_samples") %>%
+    select(
+      "hospital_name_el_short",
+      "new_samples",
+      "hospitalized_current",
+      "hospitalized_positive",
+      "hospitalized_negative",
+      "hospitalized_pending_result",
+      "home_restriction_current",
+      "new_recoveries"
+      ) %>%
     rename(
       "Νοσοκομείο" = "hospital_name_el_short",
+      "Έλεγχοι δειγμάτων" = "new_samples",
       "Νοσηλευόμενοι" = "hospitalized_current",
+      "Νοσηλευόμενοι (θετικοί)" = "hospitalized_positive",
+      "Νοσηλευόμενοι (αρνητικοί)" = "hospitalized_negative",
+      "Νοσηλευόμενοι (εν αναμονή αποτελεσμάτων)" = "hospitalized_pending_result",
       "Σε κατ'οίκον περιορισμό" = "home_restriction_current",
-      "Εξιτήρια" = "new_recoveries",
-      "Έλεγχοι δειγμάτων" = "new_samples"
+      "Εξιτήρια" = "new_recoveries"
       ) %>%
     as.data.frame()
 }
@@ -44,7 +60,10 @@ getSummaryDT_west_macedonia <- function(data) {
       scrollCollapse = T,
       dom            = 'ft',
       paging         = FALSE,
-      searching      = FALSE
+      searching      = FALSE,
+      language       = list(
+        zeroRecords  = "Δεν βρέθηκαν δεδομένα"
+      )
     ),
     selection = "none"
   )
