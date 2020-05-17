dd03956da3dd01d67a4549af5cdee97e9c6675f8
George Vlahavas <vlahavas@gmail.com>
2020-05-17 15:33:27 +0300

    Add hashtags per day wordcloud (with slider)

--- sections/content_twitter/plots.R
+++ sections/content_twitter/plots.R
@@ -5,3 +5,13 @@ output$twitter_hashtags <- renderWordcloud2({
                backgroundColor = "#444B55")
   return(w)
 })
+
+output$twitter_hashtags_per_day <- renderWordcloud2({
+  w <- data_twitter_hashtags %>%
+    filter(date == input$timeslider_twitter_hashtags) %>%
+    select(-date) %>%
+    wordcloud2a(fontFamily = "Arial",
+                color = "random-light",
+                backgroundColor = "#444B55")
+  return(w)
+})
