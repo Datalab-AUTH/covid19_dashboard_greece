e9cc7aeb6fbd9169e9787a56433907ec3ce397e4
George Vlahavas <vlahavas@gmail.com>
2020-05-17 15:33:56 +0300

    Add tweets/day plot

--- sections/content_twitter/plots.R
+++ sections/content_twitter/plots.R
@@ -15,3 +15,39 @@ output$twitter_hashtags_per_day <- renderWordcloud2({
                 backgroundColor = "#444B55")
   return(w)
 })
+
+output$twitter_tweets_per_day <- renderPlotly({
+  p <- plot_ly(
+    data = data_date_tweets,
+    x = ~date,
+    y = ~tweets,
+    type = 'scatter',
+    mode = 'lines',
+    name = "Tweets"
+  ) %>%
+  layout(
+    yaxis = list(title = "Αριθμός tweets", rangemode = "nonnegative"),
+    xaxis = list(
+      title = "Ημερομηνία",
+      type = "date",
+      tickformat = "%d/%m/%y"
+    )
+  )
+
+  p <- layout(p,
+              font = list(color = "#FFFFFF"),
+              paper_bgcolor = "#444B55",
+              plot_bgcolor = "#444B55",
+              yaxis = list(
+                zerolinecolor = "#666666",
+                linecolor = "#999999",
+                gridcolor = "#666666"
+              ),
+              xaxis = list(
+                zerolinecolor = "#666666",
+                linecolor = "#999999",
+                gridcolor = "#666666"
+              )
+  )
+  return(p)
+})
