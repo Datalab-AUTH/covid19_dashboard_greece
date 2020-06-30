dd105f6d6eaea734aaf9e1b5d3b8e8fd31a0203f
George Vlahavas <vlahavas@gmail.com>
2020-07-01 01:01:04 +0300

    Restructure key figures
    Remove the active and recovered key figures as they are not updated
    properly. Also show the actual number of new cases for confirmed,
    deceased and tests.

--- sections/content_greece/keyfigures.R
+++ sections/content_greece/keyfigures.R
@@ -5,32 +5,32 @@ key_figures_greece <- reactive({
   data_yesterday <- data_greece_all %>%
     slice(n() - 1)
   
+  data_diff <- list(
+    confirmed = data$confirmed - data_yesterday$confirmed,
+    deaths    = data$deaths - data_yesterday$deaths,
+    icu       = data$icu - data_yesterday$icu,
+    tests     = data$tests - data_yesterday$tests
+  )
+  
   data_new <- list(
-    new_confirmed = (data$confirmed - data_yesterday$confirmed) / data_yesterday$confirmed * 100,
-    new_active    = (data$active - data_yesterday$active) / data_yesterday$active * 100,
-    new_recovered = (data$recovered - data_yesterday$recovered) / data_yesterday$recovered * 100,
-    new_deaths    = (data$deaths - data_yesterday$deaths) / data_yesterday$deaths * 100,
-    new_icu       = (data$icu - data_yesterday$icu) / data_yesterday$icu * 100,
-    new_tests     = (data$tests - data_yesterday$tests) / data_yesterday$tests * 100
+    new_confirmed = data_diff$confirmed / data_yesterday$confirmed * 100,
+    new_deaths    = data_diff$deaths / data_yesterday$deaths * 100,
+    new_icu       = data_diff$icu / data_yesterday$icu * 100,
+    new_tests     = data_diff$tests / data_yesterday$tests * 100
   )
   
   keyFigures <- list(
-    "confirmed" = HTML(paste(format(data$confirmed, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_confirmed))),
-    "active"    = HTML(paste(format(data$active, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_active))),
-    "recovered" = HTML(paste(format(data$recovered, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_recovered))),
-    "deceased"  = HTML(paste(format(data$deaths, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_deaths))),
+    "confirmed" = HTML(paste(format(data$confirmed, big.mark = " "), sprintf("<h4>%+i (%+.1f %%)</h4>", data_diff$confirmed, data_new$new_confirmed))),
+    "deceased"  = HTML(paste(format(data$deaths, big.mark = " "), sprintf("<h4>%+i (%+.1f %%)</h4>", data_diff$deaths, data_new$new_deaths))),
     "icu"       = HTML(paste(format(data$icu, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_icu))),
-    "tests"     = HTML(paste(format(data$tests, big.mark = " "), sprintf("<h4>(%+.1f %%)</h4>", data_new$new_tests))),
+    "tests"     = HTML(paste(format(data$tests, big.mark = " "), sprintf("<h4>%+i (%+.1f %%)</h4>", data_diff$tests, data_new$new_tests))),
     "case_age"  = HTML(paste(format(data_greece_age_averages[["case"]], big.mark = " "), sprintf("<h4>έτη</h4>"))),
     "death_age" = HTML(paste(format(data_greece_age_averages[["death"]], big.mark = " "), sprintf("<h4>έτη</h4>"))),
     "date"      = data$date
   )
   
-  if (is.infinite(data_new$new_active)) keyFigures$active = HTML(paste(format(data$active, big.mark = " "), "<h4>(όλα νέα)</h4>"))
   if (is.infinite(data_new$new_icu)) keyFigures$icu = HTML(paste(format(data$icu, big.mark = " "), "<h4>(όλοι νέοι)</h4>"))
   if (data_new$new_confirmed == 0) keyFigures$confirmed = HTML(paste(format(data$confirmed, big.mark = " "), "<h4>(καμία αλλαγή)</h4>"))
-  if (is.nan(data_new$new_active) || data_new$new_active == 0) keyFigures$active = HTML(paste(format(data$active, big.mark = " "), "<h4>(καμία αλλαγή)</h4>"))
-  if (data_new$new_recovered == 0) keyFigures$recovered = HTML(paste(format(data$recovered, big.mark = " "), "<h4>(καμία αλλαγή)</h4>"))
   if (data_new$new_deaths == 0) keyFigures$deceased = HTML(paste(format(data$deaths, big.mark = " "), "<h4>(καμία αλλαγή)</h4>"))
   if (is.nan(data_new$new_icu) || data_new$new_icu == 0) keyFigures$icu = HTML(paste(format(data$icu, big.mark = " "), "<h4>(καμία αλλαγή)</h4>"))
   if (data_new$new_tests == 0) keyFigures$tests = HTML(paste(format(data$tests_new, big.mark = " "), "<h4>(καμία αλλαγή)</h4>"))
@@ -45,25 +45,6 @@ output$valueBox_greece_confirmed <- renderValueBox({
     icon     = icon("file-medical"),
     color    = "light-blue",
     width    = NULL
-  )
-})
-
-output$valueBox_greece_active <- renderValueBox({
-  valueBox(
-    key_figures_greece()$active,
-    subtitle = "Ενεργά κρούσματα",
-    icon     = icon("stethoscope"),
-    color    = "light-blue",
-    width    = NULL
-  )
-})
-
-output$valueBox_greece_recovered <- renderValueBox({
-  valueBox(
-    key_figures_greece()$recovered,
-    subtitle = "Αναρρώσεις",
-    icon     = icon("heart"),
-    color    = "light-blue"
   )
 })
 
@@ -97,7 +78,7 @@ output$valueBox_greece_tests <- renderValueBox({
 output$valueBox_greece_age_case <- renderValueBox({
   valueBox(
     key_figures_greece()$case_age,
-    subtitle = "Μέση ηλικία επιβεβαιωμένων κρουσμάτων",
+    subtitle = "Μέση ηλικία ασθενών",
     icon     = icon("notes-medical"),
     color    = "light-blue"
   )
@@ -106,7 +87,7 @@ output$valueBox_greece_age_case <- renderValueBox({
 output$valueBox_greece_age_death <- renderValueBox({
   valueBox(
     key_figures_greece()$death_age,
-    subtitle = "Μέση ηλικία θανάτων",
+    subtitle = "Μέση ηλικία θανόντων",
     icon     = icon("hospital"),
     color    = "light-blue"
   )
@@ -116,20 +97,12 @@ output$box_keyFigures_greece <- renderUI(box(
   title = paste0("Βασικά στοιχεία (", strftime(key_figures_greece()$date, format = "%d/%m/%Y"), ")"),
   fluidRow(
     column(
-      valueBoxOutput("valueBox_greece_confirmed", width = 3),
-      valueBoxOutput("valueBox_greece_active", width = 3),
-      valueBoxOutput("valueBox_greece_recovered", width = 3),
-      valueBoxOutput("valueBox_greece_deceased", width = 3),
-      width = 12,
-      style = "margin-left: -20px"
-    )
-  ),
-  fluidRow(
-    column(
-      valueBoxOutput("valueBox_greece_icu", width = 3),
-      valueBoxOutput("valueBox_greece_tests", width = 3),
-      valueBoxOutput("valueBox_greece_age_case", width = 3),
-      valueBoxOutput("valueBox_greece_age_death", width = 3),
+      valueBoxOutput("valueBox_greece_confirmed", width = 2),
+      valueBoxOutput("valueBox_greece_deceased", width = 2),
+      valueBoxOutput("valueBox_greece_icu", width = 2),
+      valueBoxOutput("valueBox_greece_tests", width = 2),
+      valueBoxOutput("valueBox_greece_age_case", width = 2),
+      valueBoxOutput("valueBox_greece_age_death", width = 2),
       width = 12,
       style = "margin-left: -20px"
     )
