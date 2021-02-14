718454e9ef6c86cd4d9a971d6515d79461c9abba
George Vlahavas <vlahavas@gmail.com>
2021-02-14 23:01:57 +0200

    Remove Greece areas update script
    The data is obsolete.

--- update_greece_areas.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATA="https://raw.githubusercontent.com/Datalab-AUTH/covid19_measures_map_greece/master/data.json"
-
-wget $DATA -O data/greece_map/data-new.js &&
-	sed -i "s/^const data_namekey = //g" data/greece_map/data-new.js &&
-	mv data/greece_map/data-new.js data/greece_map/data.json
-rm -f data/greece_map/data-new.js
-
