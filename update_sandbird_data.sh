b1a1e915c25e6c83975e0e04f560a3dd03160d39
George Vlahavas <vlahavas@gmail.com>
2021-02-14 11:52:22 +0200

    Get prefecture data

--- update_sandbird_data.sh
+++ update_sandbird_data.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-CASES_URL="https://raw.githubusercontent.com/Sandbird/covid19-Greece/master/cases.csv"
-wget $CASES_URL \
+URL="https://raw.githubusercontent.com/Sandbird/covid19-Greece/master/cases.csv"
+wget $URL \
 		-O data/sandbird/cases_new.csv &&
 	mv data/sandbird/cases_new.csv data/sandbird/cases.csv
 rm -f data/sandbird/cases_new.csv
+
+URL="https://raw.githubusercontent.com/Sandbird/covid19-Greece/master/prefectures.csv"
+wget $URL \
+		-O data/sandbird/prefectures_new.csv &&
+	mv data/sandbird/prefectures_new.csv data/sandbird/prefectures.csv
+rm -f data/sandbird/prefectures_new.csv
